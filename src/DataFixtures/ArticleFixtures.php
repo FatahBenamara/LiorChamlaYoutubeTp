16a89332aede82c18828122116052477ccb8ae69
benamara <fatahbenamara87@hotmail.fr>
2020-05-23 03:31:25 +0200

    fakers, relations

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,25 +5,69 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create('fr_FR');
 
-        for ($i=1; $i <=10 ; $i++) { 
-      
-            $article=new Article;
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("conteu de l'artile n°$i")
-                    ->setImage("https://via.placeholder.com/250x350")
-                    ->setCreatedAt(new \DateTime())
-      
-                    ;
-        $manager->persist($article);
+//créé 3 catégorie
 
+for ($i=1; $i <= 3 ; $i++) { 
+    
+    $category = new Category();
+    $category   ->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph())
+             
+                ;
+    $manager->persist($category);
+    
+    //créé des articles entre 4 et 6
+
+
+
+    for ($j=1; $j <= mt_rand(4,6) ; $j++) { 
+    
+        $article=new Article;
+        $article->setTitle($faker->sentence())
+                ->setContent($faker -> realText(rand(250, 500)))
+                ->setImage($faker->imageUrl)
+                ->setCreatedAt($faker -> dateTimeBetween('-6 months'))
+                ->setCategory($category)
+    
+                ;
+    $manager->persist($article);
+
+
+        //créé des commentaire
+
+        for ($k=1; $k <= mt_rand(4,10) ; $k++) { 
             
+            $comment=new Comment;
+            $comment->setAuthor($faker->name)
+                    ->setContent($faker -> realText(rand(250, 500)))
+                    ->setCreatedAt($faker -> dateTimeBetween('-40 days', 'now'))
+                    ->setArticle($article)
+            
+            
+            ;
+
+        $manager->persist($comment);
         }
+
+    
+        
+    }
+
+
+}
+
+
+
         
         
 
